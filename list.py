2ee37caa22283d5c756156096706787f1127fdd1
Fazilath Jahaa <47672121+Syed-Fazilath@users.noreply.github.com>
2019-10-18 22:09:34 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,24 @@
+if __name__ == '__main__':
+N = int(input())
+A=[]
+for i in range(N):
+    x = list(map(str,input().split()))
+    if "insert" in x:
+        A.insert(int(x[1]),int(x[2]))
+    elif "print" in x:
+        print(A)
+    elif "remove" in x:
+        A.remove(int(x[1]))
+    elif "append" in x:
+        A.append(int(x[1]))
+    elif "sort" in x:
+        A.sort()
+    elif "pop" in x:
+        A.pop()
+    elif "reverse" in x:
+        A.reverse()
+        
+            
+
+
+
